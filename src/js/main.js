df610157e7a2b278a6d28592423a481ea594bf59
=Jarosław Janaszek <jaroslaw.janaszek@gmail.com>
2019-08-20 19:58:36 +0200

    feat: JS function

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,30 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const counter = document.querySelector('.glass__counter--js');
+const add = document.querySelector('.glass__buttons--add-js');
+const remove = document.querySelector('.glass__buttons--remove-js');
+const key = new Date().toISOString().slice(0, 10);
+let glasses = localStorage.getItem(key);
 
-console.log(`Hello world!`);
 
+if (localStorage.getItem(key)) {
+  counter.innerHTML = localStorage.getItem(key);
+} else {
+  localStorage.setItem(key, '0');
+};
+
+add.addEventListener('click', (e) => {
+  localStorage.setItem(key, ++glasses);
+  counter.innerHTML = glasses;
+}
+);
+
+remove.addEventListener('click', (e) => {
+  if (glasses > 0) {
+    localStorage.setItem(key, --glasses);
+    counter.innerHTML = glasses;
+  }
+}
+);
 
